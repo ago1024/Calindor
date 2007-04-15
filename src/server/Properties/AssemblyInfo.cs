07789fa86da62fc3728c529866f76fe22a89406e
deadwood_pl <deadwood_pl>
2007-04-15 07:37:35 +0000

    VERSION__0_3_0__R

--- src/server/Properties/AssemblyInfo.cs
+++ src/server/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Licensed under terms of LGPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyInformationalVersionAttribute ("0.3.0 RC2")]
+[assembly: AssemblyInformationalVersionAttribute ("0.3.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
